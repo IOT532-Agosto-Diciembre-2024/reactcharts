a832888a3bf9eb6784d665127e4d142ad8d238a0
Andrés Torres <atorres@applab.mx>
2024-11-11 12:15:50 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+
+interface Ads1115 {
+  id: number;
+  analog_value: number;
+  voltage: string;
+  fecha: string;
+}
 
 function App() {
+  const [data, setData] = useState<Ads1115[]>([]);
+
+  async function fetchData() {
+    console.log("FechData");
+    try {
+      const response = await axios.get("http://localhost:8000/ads1115");
+      setData(response.data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+    const interval = setInterval(() => {
+      fetchData();
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LineChart
+        width={1500}
+        height={300}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 5,
+        }}
+      >
+        <XAxis dataKey="fecha" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Line type="monotone" dataKey="analog_value" stroke="#8884d8" />
+        <Line type="monotone" dataKey="voltage" stroke="#000000" />
+      </LineChart>
     </div>
   );
 }
